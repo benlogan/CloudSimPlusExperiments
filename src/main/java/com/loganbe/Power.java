4cfea7bb64cd5fd49cd44d6ea46c5010e0e8bc20
Ben Logan <1737958+benlogan@users.noreply.github.com>
2025-01-07 22:31:58 +0000

    minor

--- src/main/java/com/loganbe/Power.java
+++ src/main/java/com/loganbe/Power.java
@@ -1,23 +1,78 @@
 package com.loganbe;
 
 import org.cloudsimplus.hosts.Host;
+import org.cloudsimplus.power.models.PowerModel;
+import org.cloudsimplus.power.models.PowerModelHostSimple;
 import org.cloudsimplus.vms.HostResourceStats;
+import org.cloudsimplus.vms.Vm;
+import org.cloudsimplus.vms.VmResourceStats;
 
 import java.util.List;
+
+import static java.util.Comparator.comparingLong;
 
 public class Power {
 
     // defines the power a Host uses, even if it's idle (in Watts)
-    public static final double STATIC_POWER = 35;
+    public static final double STATIC_POWER = 100;
 
     // max power a Host uses (in Watts)
-    public static final int MAX_POWER = 50;
+    public static final int MAX_POWER = 800;
 
     // Host power consumption (in Watts) during startup
     public static final double HOST_START_UP_POWER = 5;
 
     // Host power consumption (in Watts) during shutdown
     public static final double HOST_SHUT_DOWN_POWER = 3;
+
+    /**
+     * Prints the following information from VM's utilization stats:
+     * <ul>
+     *   <li>VM's mean CPU utilization relative to the total Host's CPU utilization.
+     *       For instance, if the CPU utilization mean of two equal VMs is 100% of their CPU, the utilization
+     *       of each one corresponds to 50% of the Host's CPU utilization.</li>
+     *   <li>VM's power consumption relative to the total Host's power consumption.</li>
+     * </ul>
+     *
+     * <p>A Host, even if idle, may consume a static amount of power.
+     * Lets say it consumes 20 W in idle state and that for each 1% of CPU use it consumes 1 W more.
+     * For the 2 VMs of the example above, each one using 50% of CPU will consume 50 W.
+     * That is 100 W for the 2 VMs, plus the 20 W that is static.
+     * Therefore we have a total Host power consumption of 120 W.
+     * </p>
+     *
+     * <p>
+     * If we compute the power consumption for a single VM by
+     * calling {@code vm.getHost().getPowerModel().getPower(hostCpuUsage)},
+     * we get the 50 W consumed by the VM, plus the 20 W of static power.
+     * This adds up to 70 W. If the two VMs are equal and using the same amount of CPU,
+     * their power consumption would be the half of the total Host's power consumption.
+     * This would be 60 W, not 70.
+     * </p>
+     *
+     * <p>This way, we have to compute VM power consumption by sharing a supposed Host static power
+     * consumption with each VM, as it's being shown here.
+     * Not all {@link PowerModel} have this static power consumption.
+     * However, the way the VM power consumption
+     * is computed here, that detail is abstracted.
+     * </p>
+     */
+    public static void printVmsCpuUtilizationAndPowerConsumption(List<Vm> vmList) {
+        vmList.sort(comparingLong(vm -> vm.getHost().getId()));
+        for (Vm vm : vmList) {
+            final var powerModel = vm.getHost().getPowerModel();
+            final double hostStaticPower = powerModel instanceof PowerModelHostSimple powerModelHost ? powerModelHost.getStaticPower() : 0;
+            final double hostStaticPowerByVm = hostStaticPower / vm.getHost().getVmCreatedList().size();
+
+            //VM CPU utilization relative to the host capacity
+            final double vmRelativeCpuUtilization = vm.getCpuUtilizationStats().getMean() / vm.getHost().getVmCreatedList().size();
+            final double vmPower = powerModel.getPower(vmRelativeCpuUtilization) - hostStaticPower + hostStaticPowerByVm; // W
+            final VmResourceStats cpuStats = vm.getCpuUtilizationStats();
+            System.out.printf(
+                    "Vm   %2d CPU Usage Mean: %6.1f%% | Power Consumption Mean: %8.0f W%n",
+                    vm.getId(), cpuStats.getMean() *100, vmPower);
+        }
+    }
 
     /**
      * The Host CPU Utilization History is only computed
@@ -53,6 +108,7 @@ public class Power {
             totalPower += watts;
         }
         System.out.println("TOTAL POWER = " + totalPower + "W");
+        System.out.println();
     }
 
 }
