f1516de88b79eafabf02aedde8432e6709711f59
Ben Logan <1737958+benlogan@users.noreply.github.com>
2025-05-27 12:25:56 +0100

    minor (unused code)

--- src/main/java/org/cloudsimplus/hosts/HostSimpleFixed.java
+++ src/main/java/org/cloudsimplus/hosts/HostSimpleFixed.java
@@ -3,7 +3,9 @@ package org.cloudsimplus.hosts;
 import org.cloudsimplus.provisioners.ResourceProvisioner;
 import org.cloudsimplus.resources.HarddriveStorage;
 import org.cloudsimplus.resources.Pe;
+import org.cloudsimplus.resources.Resource;
 import org.cloudsimplus.vms.HostResourceStatsNew;
+import org.cloudsimplus.vms.Vm;
 
 import java.util.List;
 
@@ -48,5 +50,56 @@ public class HostSimpleFixed extends HostSimple {
         // Prevent the host from becoming idle by always returning false - a workaround to a major utilisation reporting bug
         return false;
     }
+
+    /**
+     * this code is only needed when investigating VM allocation problems
+    @Override
+    public HostSuitability getSuitabilityFor(final Vm vm) {
+        final var suitability = new HostSuitability(this, vm);
+
+        suitability.setForStorage(disk.isAmountAvailable(vm.getStorage()));
+        if (!suitability.forStorage()) {
+            logAllocationError(true, vm, false, "MB", this.getStorage(), vm.getStorage());
+            if (lazySuitabilityEvaluation)
+                return suitability;
+        }
+
+        suitability.setForRam(super.getRamProvisioner().isSuitableForVm(vm, vm.getRam()));
+        if (!suitability.forRam()) {
+            logAllocationError(true, vm, false, "MB", this.getRam(), vm.getRam());
+            //System.err.println("PROBLEM!!!");
+            if (lazySuitabilityEvaluation)
+                return suitability;
+        }
+
+        suitability.setForBw(super.getBwProvisioner().isSuitableForVm(vm, vm.getBw()));
+        if (!suitability.forBw()) {
+            logAllocationError(true, vm, false, "Mbps", this.getBw(), vm.getBw());
+            if (lazySuitabilityEvaluation)
+                return suitability;
+        }
+
+        suitability.setForPes(super.getVmScheduler().isSuitableForVm(vm));
+        return suitability;
+    }
+
+    private void logAllocationError(
+            final boolean showFailureLog, final Vm vm,
+            final boolean inMigration, final String resourceUnit,
+            final Resource pmResource, final Resource vmRequestedResource) {
+        if (!showFailureLog) {
+            return;
+        }
+
+        final var migration = inMigration ? "VM Migration" : "VM Creation";
+        final var msg = pmResource.getAvailableResource() > 0 ?
+                "just " + pmResource.getAvailableResource() + " " + resourceUnit :
+                "no amount";
+        LOGGER.error(
+                "{}: {}: [{}] Allocation of {} to {} failed due to lack of {}. Required {} but there is {} available.",
+                "", getClass().getSimpleName(), migration, vm, this,
+                pmResource.getClass().getSimpleName(), vmRequestedResource.getCapacity(), msg);
+    }
+    */
 
 }
