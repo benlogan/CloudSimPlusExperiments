b6bf8e4f2e80e964b62c123c476843ffa0d36f7f
Ben Logan <1737958+benlogan@users.noreply.github.com>
2026-01-25 21:41:05 +0000

    file writing - create directory
    modified approach, so that a directory is created if it doesn't exist - enables cleaner storage of CSV exports

--- src/main/java/com/loganbe/utilities/Utilities.java
+++ src/main/java/com/loganbe/utilities/Utilities.java
@@ -4,26 +4,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Utilities {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(Utilities.class.getSimpleName());
 
-    public static void writeCsv(String csvContent, String filePath) {
+    public static void writeCsv(String csvContent, String filePathString) {
         String value = System.getenv("CSV");
         if(value.equals("false")) {
+            LOGGER.error("Bypassing CSV Export!");
             return;
         }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            writer.write(csvContent);
-            // use of file:// etc is simply to make the link clickable in console
-            LOGGER.info("CSV file written successfully to file://" + new File(filePath).getAbsolutePath());
+        Path filePath = Paths.get(filePathString);
+        try {
+            // Create parent directories if they do not exist
+            Files.createDirectories(filePath.getParent());
+
+            try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
+                writer.write(csvContent);
+                // use of file:// etc is simply to make the link clickable in console
+                //LOGGER.info("CSV file written successfully to file://" + new File(filePath).getAbsolutePath());
+            }
+
         } catch (IOException e) {
-            LOGGER.error("Error writing to file: " + e.getMessage());
+            LOGGER.error("Error writing to file", e);
         }
     }
 
